3f36a1a830aa6cda4ec7ff2355bf1a7fc00d482e
David J. Morse <webdevdjm@gmail.com>
2015-06-24 14:48:54 -0400

    Portfolio Project
    Portfolio project done in bootstrap

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+
+  $('.portfolio-box').hover(
+     function () {
+       $(this).find('img').stop().fadeTo('slow', 0.4);
+       $(this).find('.caption').fadeIn(500);
+     },
+     function () {
+       $(this).find('img').stop().fadeTo('slow', 1.0);
+       $(this).find('.caption').fadeOut(250);
+     });
+
+});
